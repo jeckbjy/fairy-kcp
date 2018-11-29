c8618bdf93a4d0c6347fcc5f66c65bad1e92a1d3
jeckbjy-work <jinyuan.bian@zenjoy.net>
2018-11-30 00:08:14 +0800

    add option

--- kcp/tran.go
+++ kcp/tran.go
@@ -17,12 +17,35 @@ func NewTran() fairy.ITran {
 type kcpTran struct {
 }
 
-func (kt *kcpTran) Connect(host string) (net.Conn, error) {
-	return kcpgo.DialWithOptions(host, nil, 10, 3)
+func (kt *kcpTran) Connect(host string, options ...fairy.Option) (net.Conn, error) {
+	var kop *KcpOption
+	for _, op := range options {
+		if t, ok := op.(*KcpOption); ok {
+			kop = t
+			break
+		}
+	}
+	if kop != nil {
+		return kcpgo.DialWithOptions(kop.Addr, kop.Block, kop.DataShards, kop.ParityShards)
+	} else {
+		return kcpgo.Dial(host)
+	}
 }
 
-func (kt *kcpTran) Listen(host string) (net.Listener, error) {
-	return kcpgo.ListenWithOptions(host, nil, 10, 3)
+func (kt *kcpTran) Listen(host string, options ...fairy.Option) (net.Listener, error) {
+	var kop *KcpOption
+	for _, op := range options {
+		if t, ok := op.(*KcpOption); ok {
+			kop = t
+			break
+		}
+	}
+	if kop != nil {
+		return kcpgo.ListenWithOptions(kop.Addr, kop.Block, kop.DataShards, kop.ParityShards)
+	} else {
+		// return kcpgo.ListenWithOptions(host, nil, 10, 3)
+		return kcpgo.Listen(host)
+	}
 }
 
 func (kt *kcpTran) Serve(l net.Listener, cb base.OnAccept) {
@@ -30,4 +53,17 @@ func (kt *kcpTran) Serve(l net.Listener, cb base.OnAccept) {
 		conn, err := l.Accept()
 		cb(conn, err)
 	}
+}
+
+// KcpOption 配置选项
+type KcpOption struct {
+	Addr         string
+	Block        kcpgo.BlockCrypt
+	DataShards   int
+	ParityShards int
+}
+
+// WithKcpOption 创建KcpOption
+func WithKcpOption(addr string, block kcpgo.BlockCrypt, dataShards int, parityShards int) *KcpOption {
+	return &KcpOption{Addr: addr, Block: block, DataShards: dataShards, ParityShards: parityShards}
 }
